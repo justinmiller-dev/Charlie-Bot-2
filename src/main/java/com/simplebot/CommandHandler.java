9e707bdae020b20cde4a0762d759d0b832b29c9c
Matthew Jackson <jacksonmatt1024+github@gmail.com>
2024-10-11 12:10:48 -0500

    just some cleanup

--- src/main/java/com/simplebot/CommandHandler.java
+++ src/main/java/com/simplebot/CommandHandler.java
@@ -9,43 +9,42 @@ public class CommandHandler extends TelegramBot {
         String user = update.getMessage().getFrom().getFirstName();
         String messageText = update.getMessage().getText();
         Charmagotchi charmagotchi = Charmagotchi.getInstance(chatId);
-        if (messageText.contains("/startCharmagotchi")&& user.equals("Justin")) {
-            if (!charmagotchi.isAlive()){
+        if (messageText.contains("/startCharmagotchi") && user.equals("Justin")) {
+            if (!charmagotchi.isAlive()) {
                 charmagotchi.setBotChatId(chatId);
                 charmagotchi.startCharmagotchi();
-                sendMessage("Charmagotchi has been started",chatId);
+                sendMessage("Charmagotchi has been started", chatId);
             } else if (charmagotchi.isAlive()) {
                 sendMessage("Charlie is already running", chatId);
             }
-        } else if (messageText.contains("/startCharmagotchi")){
-            sendMessage("Silly " + user + " you're not Justin",chatId);
+        } else if (messageText.contains("/startCharmagotchi")) {
+            sendMessage("Silly " + user + " you're not Justin", chatId);
         }
-        if (messageText.contains("/playball") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/playball") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.playBall();
         }
-        if (messageText.contains("/stats")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/stats") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.showStats();
         }
-        if (messageText.contains("/meal")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/meal") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.feedMeal();
         }
-        if (messageText.contains("/treat")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/treat") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.giveTreat();
         }
-        if (messageText.contains("/speak")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/speak") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.speak();
         }
-        if (messageText.contains("/sleep")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/sleep") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.sendToBed();
         }
-        if (messageText.contains("/walk")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/walk") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             charmagotchi.goForWalk();
         }
-        if (messageText.contains("/killCharlie")&& charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId){
+        if (messageText.contains("/killCharlie") && charmagotchi.isAlive() && charmagotchi.getBotChatId() == chatId) {
             //charmagotchi.killCharlie();
-            sendMessage("Rude",chatId);
+            sendMessage("Rude", chatId);
         }
-
     }
 }
 
--- src/main/java/com/simplebot/MessageHandler.java
+++ src/main/java/com/simplebot/MessageHandler.java
@@ -2,16 +2,16 @@ package com.simplebot;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class MessageHandler extends CommandHandler{
+public class MessageHandler extends CommandHandler {
     String messageText;
     long chatID;
 
-    public void messageParse(Update update){
+    public void messageParse(Update update) {
 
         chatID = update.getMessage().getChatId();
         messageText = update.getMessage().getText();
 
-        if (messageText.startsWith("/")){
+        if (messageText.startsWith("/")) {
             commandParse(update);
         }
     }
